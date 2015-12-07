432ad9338ed86a3f73bef90918a43a5517773a5b
bendoane <ben.doane@gmail.com>
2015-12-07 08:05:17 -0500

    validates file type

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,6 +1,22 @@
 class Gif < ActiveRecord::Base
 
 belongs_to :users
+validates_format_of :url, :with => /\Ahttp(|s):\/\//
+validates_format_of :url, :with => /\.gif/
 
+before_validation :add_http
+before_validation :add_gif
+
+  def add_http
+    unless self.url =~ /\Ahttp(|s):\/\//
+      self.url = "http://" + self.url
+    end
+  end
+
+  def add_gif
+    unless self.url =~ /\.gif/
+      self.url = self.url + ".gif"
+    end
+  end
 
 end
